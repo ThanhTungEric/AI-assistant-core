33a8284d6d7ac6dacbaca1bc5cc87ce1474d5070
Tsumiki <93415945+nhuhuynh1508@users.noreply.github.com>
2025-05-23 11:18:17 +0700

    Create user entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from "typeorm";
+
+@Entity("user")
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true})
+    email: string;
+
+    @Column()
+    password: string;
+
+    // automatically set the timestamp when the user is created
+    @CreateDateColumn()
+    createdAt: Date;
+
+    // automatically set the timestamp when the user is updated
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
+
+
+
+
